6e3646c4bd2be9120e206e362fff7688c415f539
don-v <54256066+don-v@users.noreply.github.com>
2022-04-29 22:08:06 -0400

    Scott_JSEverywhere: Ch05 -- set up mongodb cluster for local!

---
+++ Scott_2020_JSEverywhere/Notes/Ch05_Database.js
@@ -0,0 +1,27 @@
+/* 
+
+// DATABASE
+
+At its core, a database allows one to store information and 
+retrieve it later!
+
+Teach has used SQL, but in this book, will use MongoDB:
+
+https://www.mongodb.com
+
+since it is a popular choice in the Node.js ecosystem. MongoDB
+stores data in 'documents' which are organized very much like 
+JS objects! Meaning, a JS dev will be able to read and write to
+MongoDB with relative ease because of MongoDB's use of JS=like 
+objects!
+
+Teach says thought, that one can use another DB if preferred,
+like 'PostgresSQL' since the concepts covered in this book
+are transferable to any system with a little work!
+
+Before we can work with Mongo, however, we will need to
+snesure that the MongoDB server is running locally. This
+is something that is required throughout developement. To
+do so, one will need to check for instructions in Ch. 1!
+
+*/
