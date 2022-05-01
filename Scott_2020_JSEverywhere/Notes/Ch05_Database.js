3310e741d4f71c06b8c6a209237ce7a3abcf39d5
don-v <54256066+don-v@users.noreply.github.com>
2022-04-30 20:59:53 -0400

    Scott_JSEverywhere: Ch05 -- getting started with mongodb!

--- Scott_2020_JSEverywhere/Notes/Ch05_Database.js
+++ Scott_2020_JSEverywhere/Notes/Ch05_Database.js
@@ -24,4 +24,40 @@ snesure that the MongoDB server is running locally. This
 is something that is required throughout developement. To
 do so, one will need to check for instructions in Ch. 1!
 
+```
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://kj1905:<password>@notes-graphql.6z3u3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, 
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true, 
+        serverApi: ServerApiVersion.v1 
+    });
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+```
+
+```
+mongodb+srv://kj1905:<password>@notes-graphql.6z3u3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+```
+
+"""
+Replace <password> with the password for the kj1905 user. 
+Replace myFirstDatabase with the name of the database that 
+connections will use by default. Ensure any option params 
+are URL encoded.
+"""
+
+// GETTING STARTED WITH MONGODB
+
+teach uses a the 'mongo' executable in the shell, but i'll just follow along
+since I don't want to install more software on my machine!
+
+
 */
