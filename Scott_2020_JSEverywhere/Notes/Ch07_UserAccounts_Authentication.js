73dd5bd2b204c73e8e7b86dac03182b15fd32e21
don-v <54256066+don-v@users.noreply.github.com>
2022-05-28 22:00:20 -0400

    Scott_JSEverywhere: Ch07 -- started p. 55!

---
+++ Scott_2020_JSEverywhere/Notes/Ch07_UserAccounts_Authentication.js
@@ -0,0 +1,17 @@
+/* 
+// CHAPTER 7: USER ACCOUNTS AND AUTHENTICATION
+
+We supply username and password -- submit the form to the 'receptionist'.
+'recpetionist' then encrypts the password with a secret key and stores the 
+encrypted version of the password in a locked file vault.
+
+The 'receptionist' then stamps a coin, on which is pressed one's unique
+membership ID. Upon returning to the front room, the 'receptionist'
+hand you the coinc, which you tuck away in yoru pocket. Now each time
+you return to the club, one only needs to show the coin to gain entrance!
+
+HERE p. 55!
+
+
+
+*/
