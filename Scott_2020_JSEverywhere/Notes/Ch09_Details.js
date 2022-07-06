47b39e172f48efaeb8858851c0c47d755313d575
don-v <54256066+don-v@users.noreply.github.com>
2022-07-06 00:26:14 -0400

    Scott_JSEverywhere: Ch09 -- web app, express.js best prax, p. 83!

---
+++ Scott_2020_JSEverywhere/Notes/Ch09_Details.js
@@ -0,0 +1,14 @@
+/*
+// CH09: DETAILS
+
+So far, we have a working API, but it's lacking the finishing 
+touches that will allows us to put it into production. In this
+chapter we'll implement some web and GraphQL application security
+and user experience best practices. These details will be critical to 
+safety, security, and usability of our application.
+
+// WEB APPLICATION AND EXPRESS.JS BEST PRAX...
+
+# HERE -- p. 83!
+
+*/
