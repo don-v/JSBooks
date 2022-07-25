b5acef3f1300b87702e590b0e7058eec601667ea
don-v <54256066+don-v@users.noreply.github.com>
2022-07-24 21:31:44 -0400

    Scott_JSEverywhere: C11 -- React hx, p. 103!

--- Scott_2020_JSEverywhere/Notes/Ch11_UI_React.js
+++ Scott_2020_JSEverywhere/Notes/Ch11_UI_React.js
@@ -31,6 +31,43 @@ As apps grew, so did need for organization and boilerplate
 code! By early 2010s, frameworks like 'Backbone', 'Angular',
 and 'Ember' dominated the JS application landscape.
 
-# HERE -- p. 102!
+These frameworks imposed application structure, and 
+provided code that facilitated the implementation of
+common application patterns; These frameworks were
+often modeled after the Model, Controller, and View
+(MVC) software design pattern. Each framework was 
+prescriptive about all of the layers of the web application,
+providing a structured ways to handle templating, data,
+and user interactions. While this approach had many
+benefits, it also meant that the effort of intergrating
+new or non-standard technologies could be quite high!
+
+Meanwhile, desktop applications conitnued to be written
+in system-specific programming languages. This mean that 
+developers and teams were often forced to make either/or
+style choices (either a Mac app or a Windows app), (either
+as web app or a desktop app); 
+
+Mobile applications were in a similar position; The rise
+of responsive web design meant that designers and devs
+could create truly incredible sites and applications for
+the mobile web browser, but choosing to build a web-only
+application locked them out of the mobile platform app 
+stores.
+
+Applies' iOS applicaitons were written in Objective-C
+(and more recently Swift), while Android relied upon
+Java. This meant that the web, consisting of 
+HTML, CSS, and JS was the only truly cross-platform
+UI environment!
+
+// DECLARATIVE INTERFACES WITH JS
+
+In early 2010s, devs at FB began to face challenges in the
+organization, and management of their JS code. In response,
+the software engineer Jordan Walke wrote React, inspired
+by FB's PHP library, XHP. React...
+
+# HERE -- p. 103!
 
 */
