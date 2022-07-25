7d653cc4e9e271328662c95fbd657ab77906f908
don-v <54256066+don-v@users.noreply.github.com>
2022-07-24 21:19:29 -0400

    Scott_JSEverywhere: C11 -- JS and UIs hx, p. 102!

---
+++ Scott_2020_JSEverywhere/Notes/Ch11_UI_React.js
@@ -0,0 +1,36 @@
+/* 
+USER INTERFACES AND REACT
+
+In this chapter, we explore we take a brief look at the
+history of JS UI development. 
+
+With that background, we then explore React, the
+JS library we'll be using the rest of the way!
+
+JS developed in mid-1990s (in 10 days, allegedly); JS provided
+an embedded scripting language; allowed devs to add small
+interactions to web page that were not possible with HTML
+alone!
+
+early, there were issues with cross-browser compatibility
+which led to the proliferation of the dev of many browser-
+specific libraries!
+
+Mid-2000s, 'jQuery` and 'MooTools' took off; jQuery allowed
+devs to write JS with a simple API that worked well across
+browsers. Facilitated the ability to remove, add, replace, 
+and animate things on web pages!
+
+Around the same time, AJAX (asynchronous JS and XML) came 
+on scene; AJAX allowed one to fetch data from a server and
+inject it into the page. The combinations of these 2 
+technologies provided an ecosystem to create powerful
+interactive web applications!
+
+As apps grew, so did need for organization and boilerplate
+code! By early 2010s, frameworks like 'Backbone', 'Angular',
+and 'Ember' dominated the JS application landscape.
+
+# HERE -- p. 102!
+
+*/
