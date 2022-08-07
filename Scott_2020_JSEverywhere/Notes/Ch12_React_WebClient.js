60b6eec87051af38d8b107f7950e5df14ceab76f
don-v <54256066+don-v@users.noreply.github.com>
2022-08-06 22:27:55 -0400

    Scott_JSEverywhere: C12 -- what we're building, p. 109!

---
+++ Scott_2020_JSEverywhere/Notes/Ch12_React_WebClient.js
@@ -0,0 +1,37 @@
+/* 
+https://github.com/javascripteverywhere
+
+// CHAPTER 12: BUILDING A WEB CLIENT WITH REACT
+
+The original idea behind hypertext was to take related documents and link
+them together: if academic paper A references academic paper B let's make
+it easy to click something and navigate between them. In 1989, a software 
+engineer at CERN named Tim Berners-Lee had the idea to combine hypertext with 
+networked computers, making it easy for someone to make these connections 
+regardless of the locaiton of the documents. 
+
+At its heart, the web remaines a medium for linking documents together. 
+Each page is HTML, rendered in a web browser, with CSS  for styling and
+JS for enhancements. Today, we use these technologies to build everything
+from personal blogs and small brochure sites to complex interactive
+applicaitons. the underlying advantage is that the web provides universal
+access.  All anyone needs is a web browser on a web-connected device,
+creating an inclusive-by-default environment.
+
+// WHAT WE WILL BUILD WITH TEACH!
+
+In the upcoming chapters we'll build the web client for our social
+note application, Notedly. Users will be able to create and sign
+in to an account, write notes in Markdown, edit their notes, view a
+feed of other users' notes, and 'favorite' other users' notes. To
+accomplish all of this, we will be interacting with out GraphQL
+server API.
+
+In our web application, users will be able to:
+
+* create notes, as well as read, update, and delete notes they've created!
+
+* view a feed...
+
+# HERE p. 109!
+*/
