a14dc3817e4a9c1d80852d53139827329da12cba
don-v <54256066+don-v@users.noreply.github.com>
2022-08-24 20:01:17 -0400

    Scott_JSEverywhere: C13 -- Creating a Layout Component

---
+++ Scott_2020_JSEverywhere/Notes/Ch13_App_Styling.js
@@ -0,0 +1,26 @@
+/* CHAPTE 13: STYLING AN APPLICATION
+
+teach argues that style is an important part of human culture (nature).
+Clothes not only serve the functional purpose of protecting our bodies,
+but also convey information about our culture, social status, interests,
+and much more!
+
+A web application is functional without anything beyondthe deafult styles
+of the web, but by applying CSS we are able to more clearly communicate 
+with out users.
+
+Inthis chapter, we will explore how we can use the 'CSs-in-JS' Styled
+Components library to introduce layout and style to our application. This
+will allow us to make a more usable and aesthetically pleasing application
+within a maintainable, component-based, code structure.
+
+// CREATING A LAYOUT COMPONENT
+
+Many, or in our case all, pages of an application will share a common
+layout. for example, all the pages of our application will have a header,
+a sidebar, and a content area (Figure 13-1). Rather than import the
+shared layout 
+
+# HERE -- p.121!
+
+*/
