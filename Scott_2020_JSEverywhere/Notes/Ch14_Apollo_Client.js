fe7d58ad4caec47135892cea2d646af535b05620
don-v <54256066+don-v@users.noreply.github.com>
2022-09-08 07:05:38 -0400

    Scott_JSEverywhere: started C14, p. 136!

---
+++ Scott_2020_JSEverywhere/Notes/Ch14_Apollo_Client.js
@@ -0,0 +1,24 @@
+/* 
+// CHAPTER 14: WORKING IWTH APOLLO CLIENT
+
+The modem-based connections for internet access from previous
+decades, though quite slow and unreliable, still capture the 
+essence of modern-day service communications:
+
+1. services request a connection
+2. make the connection
+3. send a request
+4. recieve the response
+
+Our client application will work in a similar manner. We will
+first make a connection to our server API application, and if
+successful, make requests to that server.
+
+In this chapter, we will use Apollo Client to connect to our
+API. Once connected, we'll write a GraphQL query, which will be
+used to display data on a page. We'll also introduce pagination, 
+both within the API query and in our interface components!
+
+# HERE -- p. 136!
+
+*/
