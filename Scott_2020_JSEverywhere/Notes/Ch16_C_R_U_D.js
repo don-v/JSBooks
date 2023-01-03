585fdd6b26c509c19a9f3bf0ae5449634879898d
don-v <54256066+don-v@users.noreply.github.com>
2023-01-03 12:15:32 -0500

    Scott_JSEverywhere: C16 -- updated src.pages.favorites, p. 186!

--- Scott_2020_JSEverywhere/Notes/Ch16_C_R_U_D.js
+++ Scott_2020_JSEverywhere/Notes/Ch16_C_R_U_D.js
@@ -569,8 +569,43 @@ const GET_MY_FAVORITES = gql`
 export { GET_NOTES, GET_NOTE, IS_LOGGED_IN, GET_MY_NOTES, GET_MY_FAVORITES };
 ```
 
-Now, in '/src/pages/favorites.js':
+Now, in '/src/pages/favorites.js', we update the source as follows:
 
-# HERE -- p. 185!
+```
+import React, { useEffect } from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+import NoteFeed from '../components/NoteFeed';
+
+// import the query
+import { GET_MY_FAVORITES } from '../gql/query';
+
+const Favorites = () => {
+  useEffect(() => {
+    // update the document title
+    document.title = 'Favorites - Notedly';
+  });
+
+  const { loading, error, data } = useQuery(GET_MY_FAVORITES);
+
+  // if the data is loading, our app will display a loading message
+  if (loading) return 'Loading...';
+  // if there is an error fetching the data, display an error message
+  if (error) return `Error! ${error.message}`;
+  // if the query is successful and there are notes, return the feed of notes
+  // else if the query is successful and there aren't notes, display a message
+  if (data.me.favorites.length !== 0) {
+    return <NoteFeed notes={data.me.favorites} />
+  } else {
+    return <p>No favorites yet</p>;
+  }
+  
+};
+
+export default Favorites;
+
+```
+
+# HERE -- P. 186!
 
 */
