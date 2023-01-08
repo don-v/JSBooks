c121fbfb1220e388c293a07d9e61aa5df672cea1
don-v <54256066+don-v@users.noreply.github.com>
2023-01-08 12:30:10 -0500

    Scott_JSEverywhere: C16 -- EditNote page, p. 187!

--- Scott_2020_JSEverywhere/Notes/Ch16_C_R_U_D.js
+++ Scott_2020_JSEverywhere/Notes/Ch16_C_R_U_D.js
@@ -702,7 +702,30 @@ begin, we can make a duplicate of our 'src/pages/notes.js' page
 and name it 'edit.js'. For now, this page will simply display
 the note.
 
-# HERE -- p. 186!
+in '/src/pages/edit.js':
+
+```
+import React from 'react';
+import { useQuery, useMutation, gql } from '@apollo/client'
+
+// import the Note component
+import Note from '../components/Note';
+// import the `GET_NOTE` query
+import { GET_NOTE } from '../gql/query';
+
+const EditNote = props => {
+  // store the id found in the url as a variable
+  const id = props.match.params.id;
+  // define our note query
+  const { loading, error, data } = useQery(GET_NOTE, { variables: { id }});
+
+  // if the data is loading, display a loading message
+  if (loading) return 'Loading...';
+  // if there is an error fetching the data, display an error message
+  if (error) return <p>Error! Note not found</p>;
+  // HERE -- p. 187!
+}
+```
 
 {
     username: 'world_lover2',
--- Scott_2020_JSEverywhere/_web-master/src/pages/edit.js
+++ Scott_2020_JSEverywhere/_web-master/src/pages/edit.js
@@ -1,42 +1,20 @@
 import React from 'react';
-// import GraphQL dependencies
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, useMutation, gql } from '@apollo/client'
 
 // import the Note component
 import Note from '../components/Note';
+// import the `GET_NOTE` query
+import { GET_NOTE } from '../gql/query';
 
-// the note query, which accepts an ID variable
-const GET_NOTE = gql`
-  query note($id: ID!) {
-    note(id: $id) {
-      id
-      createdAt
-      content
-      favoriteCount
-      author {
-        username
-        id
-        avatar
-      }
-    }
-  }
-`;
-
-const NotePage = props => {
-  // store the id found in the url a a variable
+const EditNote = props => {
+  // store the id found in the url as a variable
   const id = props.match.params.id;
-
-  // query hook, passing the id value as a variable
-  const { loading, error, data } = useQuery(GET_NOTE, { variables: { id } });
+  // define our note query
+  const { loading, error, data } = useQery(GET_NOTE, { variables: { id }});
 
   // if the data is loading, display a loading message
-  if (loading) return <p>Loading...</p>;
-
+  if (loading) return 'Loading...';
   // if there is an error fetching the data, display an error message
   if (error) return <p>Error! Note not found</p>;
-
-  // if the data is successful, display the data in our UI
-  return <Note note={data.note} />;
-};
-
-export default NotePage;
+  // HERE -- p. 187!
+}
