1aa917efbadf220ac3a309b9e1eb72ac112df2ac
don-v <54256066+don-v@users.noreply.github.com>
2023-01-15 12:58:34 -0500

    Scott_JSEverywhere: C16 -- updated edit.js with user info query, p. 188!

--- Scott_2020_JSEverywhere/Notes/Ch16_C_R_U_D.js
+++ Scott_2020_JSEverywhere/Notes/Ch16_C_R_U_D.js
@@ -973,7 +973,45 @@ we just implemented and include a user check:
 
 # HERE -- p. 188!
 
+after updating '/src/pages/edit.js', our source looks as follows:
 
+```
+import React from 'react';
+import { useQuery, useMutation, gql } from '@apollo/client'
+
+// import the Note component
+// import Note from '../components/Note';
+
+// import the NoteForm component
+import NoteForm from '../components/NoteForm';
+
+// import the queries from query
+import { GET_NOTE, GET_ME } from '../gql/query';
+import { EDIT_NOTE } from '../gql/mutation';
+
+const EditNote = props => {
+  // store the id found in the url as a variable
+  const id = props.match.params.id;
+  // define our note query
+  const { loading, error, data } = useQuery(GET_NOTE, { variables: { id }});
+  // fetch the current user's data
+  const { data: userdata } = useQuery(GET_ME);
+  // if the data is loading, display a loading message
+  if (loading) return 'Loading...';
+  // if there is an error fetching the data, display an error message
+  if (error) return <p>Error! Note not found</p>;
+  // if the current user and the author of the note do not match
+  if (userdata.me.id !== data.note.author.id) {
+    return <p>You do not have access to edit this note</p>;
+  }
+  // pass the data to the form component
+  return <NoteForm content={data.note.content} />;
+};
+
+export default EditNote;
+```
+
+// HERE -- p. 188!
 
 {
     username: 'world_lover2',
--- Scott_2020_JSEverywhere/_web-master/src/pages/edit.js
+++ Scott_2020_JSEverywhere/_web-master/src/pages/edit.js
@@ -7,21 +7,27 @@ import { useQuery, useMutation, gql } from '@apollo/client'
 // import the NoteForm component
 import NoteForm from '../components/NoteForm';
 
-// import the `GET_NOTE` query
-import { GET_NOTE } from '../gql/query';
+// import the queries from query
+import { GET_NOTE, GET_ME } from '../gql/query';
+import { EDIT_NOTE } from '../gql/mutation';
 
 const EditNote = props => {
   // store the id found in the url as a variable
   const id = props.match.params.id;
   // define our note query
   const { loading, error, data } = useQuery(GET_NOTE, { variables: { id }});
-
+  // fetch the current user's data
+  const { data: userdata } = useQuery(GET_ME);
   // if the data is loading, display a loading message
   if (loading) return 'Loading...';
   // if there is an error fetching the data, display an error message
   if (error) return <p>Error! Note not found</p>;
-  // if successful, pass the data to the note coomponent
-  return <Note note={data.note} />;
+  // if the current user and the author of the note do not match
+  if (userdata.me.id !== data.note.author.id) {
+    return <p>You do not have access to edit this note</p>;
+  }
+  // pass the data to the form component
+  return <NoteForm content={data.note.content} />;
 };
 
 export default EditNote;
