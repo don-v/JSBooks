c6bc3118f7223404da99b3f0145c576264466659
don-v <54256066+don-v@users.noreply.github.com>
2023-02-09 13:29:57 -0500

    Scott_JSEverywhere: C16 -- updated FavoriteNote with toggle, getfavorites refetch, p. 198!

--- Scott_2020_JSEverywhere/Notes/Ch16_C_R_U_D.js
+++ Scott_2020_JSEverywhere/Notes/Ch16_C_R_U_D.js
@@ -1659,8 +1659,66 @@ Then we go back and update our 'FavoriteNote' compoentns
 in the file '/src/components/FavoriteNote.js' as follows:
 
 ```
-// HERE -- p. 197!
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+
+import ButtonAsLink from './ButtonAsLink';
+// import TOGGLE_FAVORITE mutation
+import { TOGGLE_FAVORITE } from '../gql/mutation';
+// add the GET_MY_FAVORITES query to refetch
+import { GET_MY_FAVORITES } from '../gql/query';
+
+const FavoriteNote = props => {
+    // store the note's favorite count as state
+    const [count, setCount]  = useState(props.favoriteCount);
+
+    // store if the user has favorited the note as state
+    const [favorited, setFavorited] = useState(
+        // check if the note exists in the user favorites list
+        props.me.favorites.filter(note => note.id === props.noteId).length > 0
+    );
+
+    // toggleFavorite mutation hook
+    const [toggleFavorite] = useMutation(TOGGLE_FAVORITE, {
+        variables: {
+            id: props.noteId
+        },
+        // refetch the GET_MY_FAVORITES query to update the cache
+        refetchQueries: [{ query: GET_MY_FAVORITES }]
+    });
+
+    // if the user has favorited the note, display the option to remove the favorite
+    // else, display the option to add as a favorite
+    return (
+        <React.Fragment>
+            {favorited ? (
+                <ButtonAsLink
+                    onClick={() => {
+                        setFavorited(false);
+                        setCount(count - 1);
+                    }}                
+                >
+                    Remove Favorite
+                </ButtonAsLink>
+            ) : (
+                <ButtonAsLink
+                    onClick={() => {
+                        setFavorited(true);
+                        setCount(count + 1);
+                    }}
+                >
+                    Add Favorite
+                </ButtonAsLink>
+            )}
+            : {count}
+        </React.Fragment>
+    );
+};
+
+export default FavoriteNote;
 ```
+
+// HERE -- p. 198!
 
 {
     username: 'world_lover2',
--- Scott_2020_JSEverywhere/_web-master/src/components/FavoriteNote.js
+++ Scott_2020_JSEverywhere/_web-master/src/components/FavoriteNote.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 
 import ButtonAsLink from './ButtonAsLink';
+// import TOGGLE_FAVORITE mutation
+import { TOGGLE_FAVORITE } from '../gql/mutation';
+// add the GET_MY_FAVORITES query to refetch
+import { GET_MY_FAVORITES } from '../gql/query';
 
 const FavoriteNote = props => {
     // store the note's favorite count as state
@@ -13,6 +17,17 @@ const FavoriteNote = props => {
         props.me.favorites.filter(note => note.id === props.noteId).length > 0
     );
 
+    // toggleFavorite mutation hook
+    const [toggleFavorite] = useMutation(TOGGLE_FAVORITE, {
+        variables: {
+            id: props.noteId
+        },
+        // refetch the GET_MY_FAVORITES query to update the cache
+        refetchQueries: [{ query: GET_MY_FAVORITES }]
+    });
+
+    // if the user has favorited the note, display the option to remove the favorite
+    // else, display the option to add as a favorite
     return (
         <React.Fragment>
             {favorited ? (
