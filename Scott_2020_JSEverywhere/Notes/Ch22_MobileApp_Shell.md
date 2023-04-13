2d9a1501f27d2abb70439e092ca7c3de66928214
don-v <54256066+don-v@users.noreply.github.com>
2023-04-13 13:21:47 -0400

    Scott_JSEverywhere: C22 -- using styled components, p. 241!

--- Scott_2020_JSEverywhere/Notes/Ch22_MobileApp_Shell.md
+++ Scott_2020_JSEverywhere/Notes/Ch22_MobileApp_Shell.md
@@ -178,5 +178,36 @@ If one followed along in C13, this syntax should look very familiar. The
 final code of our _/src/Main.js_ files becomes:
 
 ```JavaScript
-// HERE -- p. 241!
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+import styled from 'styled-components/native';
+
+const StytledView = styled.View`
+  flex: 1;
+  justify-content: center;
+`;
+
+const H1 = styled.Text`
+  font-size: 48px;
+  font-weight: bold;
+`;
+
+const P = styled.Text`
+  margin: 24px 0;
+  font-size: 18px;
+`;
+
+const Main = () => {
+  return (
+    <StyledView>
+      <H1>Hello world!</H1>
+      <P>This is my app</P>
+      <Image source={require('../assets/images/hello-world.jpg')} />
+    </StyledView>
+  );
+};
+
+export default Main;
 ```
+
+<!-- HERE -- p. 241! -->
--- Scott_2020_JSEverywhere/mobile-master/src/Main.js
+++ Scott_2020_JSEverywhere/mobile-master/src/Main.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import { Text, View, Image, StyleSheet } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import styled from 'styled-components/native';
+
+const StytledView = styled.View`
+  flex: 1;
+  justify-content: center;
+`;
+
+const H1 = styled.Text`
+  font-size: 48px;
+  font-weight: bold;
+`;
+
+const P = styled.Text`
+  margin: 24px 0;
+  font-size: 18px;
+`;
 
 const Main = () => {
   return (
-    <View style={stles.container}>
-      <Text style={styles.h1}>Hello world!</Text>
-      <Text style={styles.paragraph}>This is my app</Text>
-      <Image source={require('.../assets/images/hello-world.jpg')} />
-    </View>    
+    <StyledView>
+      <H1>Hello world!</H1>
+      <P>This is my app</P>
+      <Image source={require('../assets/images/hello-world.jpg')} />
+    </StyledView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center'
-  },
-  h1: {
-    fonstSize: 48,
-    fontWeight: 'bold'
-  },
-  paragraph: {
-    marginTop: 24,
-    marginBottom: 24,
-    fontSize: 18
-  }
-});
 
 export default Main;
