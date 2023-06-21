bda62b6ac195bcb2801aada9b1e5b9c041947881
don-v <54256066+don-v@users.noreply.github.com>
2023-06-21 11:44:52 -0400

    Scott_JSEverywhere: C24 -- signUP form, p. 294!

--- Scott_2020_JSEverywhere/Notes/Ch24_MobileApp_Auth.md
+++ Scott_2020_JSEverywhere/Notes/Ch24_MobileApp_Auth.md
@@ -1136,8 +1136,63 @@ call our `signUp` GraphQL mutations and pass a `formtype="signUp"` property
 to our `UserForm` component:
 
 ```js
+import React from 'react';
+import { Text } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
+import { useMutation, gql } from '@apollo/client';
 
+import UserForm from '../components/UserForm';
+import Loading from '../components/Loading';
+
+// signUp GraphQL mutation
+const SIGNUP_USER = gql`
+    mutation signUp($email: String!, &username: String!, $password: String!) {
+        signUp(email: $email, username: $username, password: $password)
+    }
+`;
+
+// update `storeToken` function to store a token string passed as a
+// parameter
+
+const SignUp = props => {
+    // store the token with a key value of `token`
+    // after the token is stored navigate to the app's `main` screen
+    const storeToken = token => {
+        SecureStore.setItemAsync('token', token).then(
+            props.navigation.navigate('App')
+        );
+    };
+
+    // the signUp mutation hook
+    const [signUp, { loading, error }] = useMutation(SIGNUP_USER, {
+        onCompleted: data => {
+            storeToken(data.signUp)
+        }
+    })
+
+    // if loading, return a loading indicator
+    if (loading) return <Loading />;
+    
+    return (
+        <React.Fragment>
+            {error && <Text>Error signing in!</Text>}
+            <UserForm 
+                action={signUp}
+                formType="signUp"
+                navigation={props.navigation}
+            />
+        </React.Fragment>
+    );
+}
+
+SignUp.navigationOptions = {
+    title: 'Register'
+};
+
+export default SignUp;
 ```
 
-<!-- HERE -- p. 293! -->
+With the screen created, we can add it to our router...
+
+<!-- HERE -- p. 294! -->
 
--- Scott_2020_JSEverywhere/mobile-master/src/screens/signup.js
+++ Scott_2020_JSEverywhere/mobile-master/src/screens/signup.js
@@ -6,19 +6,17 @@ import { useMutation, gql } from '@apollo/client';
 import UserForm from '../components/UserForm';
 import Loading from '../components/Loading';
 
-// HERE -- p. 293!
-
-// add our GraphQL query
-const SIGNIN_USER = gql`
-    mutation signIn($email: String, $password, String!) {
-        signIn(email: $email, password: $password)
+// signUp GraphQL mutation
+const SIGNUP_USER = gql`
+    mutation signUp($email: String!, &username: String!, $password: String!) {
+        signUp(email: $email, username: $username, password: $password)
     }
 `;
 
 // update `storeToken` function to store a token string passed as a
 // parameter
 
-const SignIn = props => {
+const SignUp = props => {
     // store the token with a key value of `token`
     // after the token is stored navigate to the app's `main` screen
     const storeToken = token => {
@@ -27,28 +25,30 @@ const SignIn = props => {
         );
     };
 
-    const [signIn, { loading, error }] = useMutation(SIGNIN_USER, {
+    // the signUp mutation hook
+    const [signUp, { loading, error }] = useMutation(SIGNUP_USER, {
         onCompleted: data => {
-            storeToken(data.signIn)
+            storeToken(data.signUp)
         }
     })
 
     // if loading, return a loading indicator
     if (loading) return <Loading />;
+    
     return (
         <React.Fragment>
             {error && <Text>Error signing in!</Text>}
             <UserForm 
-                action={signIn}
-                formType="signIn"
+                action={signUp}
+                formType="signUp"
                 navigation={props.navigation}
             />
         </React.Fragment>
     );
 }
 
-SignIn.navigationOptions = {
-    title: 'Sign In'
+SignUp.navigationOptions = {
+    title: 'Register'
 };
 
-export default SignIn;
+export default SignUp;
