bbfc035e4785d17a3600dfd74dc9d0ec47841e3a
don-v <54256066+don-v@users.noreply.github.com>
2023-08-08 12:58:09 -0400

    EloquentJS: C01 -- did numbers prax!

---
+++ EloquentJS_3rdEd/P1_Language/01_ValuesTypesOperators/prax/01_ValuesTypesOperators_numbers.js
@@ -0,0 +1,11 @@
+let x = 13;
+console.log('x=',x);
+console.log('type of x:',typeof x);
+
+let y = 9.81;
+console.log('y=',y);
+console.log('type of y:',typeof y);
+
+let z = 2.998e8;
+console.log('z=',z);
+console.log('type of z:',typeof z);
