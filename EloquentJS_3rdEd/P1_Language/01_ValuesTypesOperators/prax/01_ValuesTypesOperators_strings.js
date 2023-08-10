9a2a379e241ec861371c121b5b3a35fbf62b557f
don-v <54256066+don-v@users.noreply.github.com>
2023-08-10 13:04:55 -0400

    EloquentJS: C01 -- string!

---
+++ EloquentJS_3rdEd/P1_Language/01_ValuesTypesOperators/prax/01_ValuesTypesOperators_strings.js
@@ -0,0 +1,19 @@
+const str1 = `Down on the sea`;
+const str2 = "Lie on the ocean";
+const str3 = 'Float on the ocean';
+
+console.log('str1=', str1);
+console.log('backtick str1 typeof:', typeof str1);
+
+console.log('str2=', str2);
+console.log('double quote str2 typeof:', typeof str2);
+
+console.log('str3=', str3); 
+console.log('single quote str3 typeof:', typeof str3);
+
+const newline_str = "This is the first line\nAnd this is the second";
+console.log('newline_str=', newline_str);
+console.log('newline_str type:', typeof newline_str);
+
+const escape_newline = "A newline character is written like \"\\n\".";
+// HERE -- STRINGS!
