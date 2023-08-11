cc991bcad6c9c7567e4f48eb5cab074bc620fd0c
don-v <54256066+don-v@users.noreply.github.com>
2023-08-11 13:22:57 -0400

    EloquentJS: C01 -- unary operators prax

--- EloquentJS_3rdEd/P1_Language/01_ValuesTypesOperators/prax/01_ValuesTypesOperators_strings.js
+++ EloquentJS_3rdEd/P1_Language/01_ValuesTypesOperators/prax/01_ValuesTypesOperators_strings.js
@@ -16,4 +16,24 @@ console.log('newline_str=', newline_str);
 console.log('newline_str type:', typeof newline_str);
 
 const escape_newline = "A newline character is written like \"\\n\".";
-// HERE -- STRINGS!
+console.log('escape_new line:', escape_newline);
+console.log('escape_newline typeof:', typeof escape_newline);
+
+console.log('string concatenation: "con" + "cat" + "e" + "name":', "con" + "cat" + "e" + "name");
+
+const template_lit = `half of 100 is ${100 / 2}`;
+console.log('template literal: "`half of 100 is ${100 / 2}`"', template_lit);
+
+// str1= Down on the sea
+// backtick str1 typeof: string
+// str2= Lie on the ocean
+// double quote str2 typeof: string
+// str3= Float on the ocean
+// single quote str3 typeof: string
+// newline_str= This is the first line
+// And this is the second
+// newline_str type: string
+// escape_new line: A newline character is written like "\n".
+// escape_newline typeof: string
+// string concatenation: "con" + "cat" + "e" + "name": concatename
+// template literal: "`half of 100 is ${100 / 2}`" half of 100 is 50
