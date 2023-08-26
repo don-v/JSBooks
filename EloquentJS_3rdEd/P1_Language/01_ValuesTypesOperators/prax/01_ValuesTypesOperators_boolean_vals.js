5424aa262e4fe100dec199afc144cbe2707da2ea
don-v <54256066+don-v@users.noreply.github.com>
2023-08-26 17:19:04 -0400

    EloquentJS: C01 -- boolean compar ex!

---
+++ EloquentJS_3rdEd/P1_Language/01_ValuesTypesOperators/prax/01_ValuesTypesOperators_boolean_vals.js
@@ -0,0 +1,20 @@
+// COMPARISON
+
+console.log("3 > 2:", 3 > 2)
+// → true
+console.log("3 < 2:", 3 < 2)
+// → false
+
+    // COMPARISON: STRINGS
+
+    console.log('"Aardvark" < "Zoroaster":',"Aardvark" < "Zoroaster")
+    // → true
+    console.log('"Itchy" != "Scratchy":',"Itchy" != "Scratchy")
+    // → true
+    console.log('"Apple" == "Orange":',"Apple" == "Orange")
+    // → false
+
+    // COMPARISON: EXAMPLE OF VALUE NOT EQUAL OT ITSELF:
+
+    console.log("NaN == NaN:",NaN == NaN)
+    // → false
---
+++ EloquentJS_3rdEd/P1_Language/01_ValuesTypesOperators/prax/01_ValuesTypesOperators_unary_operators.js
@@ -0,0 +1,7 @@
+console.log("typeof 4.5:",typeof 4.5);
+// → number
+console.log('typeof "x":',typeof "x");
+// → string
+
+// typeof 4.5: number
+// typeof "x": string
