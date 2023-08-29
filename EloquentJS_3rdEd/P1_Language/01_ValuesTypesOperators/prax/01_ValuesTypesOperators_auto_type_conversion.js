28252bc1f043d79e49aa54fe21679eb2926b5be0
don-v <54256066+don-v@users.noreply.github.com>
2023-08-29 12:19:10 -0400

    EloquentJS: C01 -- auto type conversion!

---
+++ EloquentJS_3rdEd/P1_Language/01_ValuesTypesOperators/prax/01_ValuesTypesOperators_auto_type_conversion.js
@@ -0,0 +1,34 @@
+console.log('8 * null:',8 * null);
+// → 0
+console.log('"5" - 1:',"5" - 1);
+// → 4
+console.log('"5" + 1:',"5" + 1);
+// → 51
+console.log('"five" * 2',"five" * 2);
+// → NaN
+console.log('false == 0:s',false == 0);
+// → true
+
+
+console.log('null == undefined:',null == undefined);
+// → true
+console.log('null == 0:',null == 0);
+// → false
+
+console.log('0 == false:',0 == false);
+console.log('"" == false:',"" == false);
+console.log('"" === false:',"" === false);
+
+
+/* 
+8 * null: 0
+"5" - 1: 4
+"5" + 1: 51
+"five" * 2 NaN
+false == 0:s true
+null == undefined: true
+null == 0: false
+0 == false: true
+"" == false: true
+"" === false: false
+*/
