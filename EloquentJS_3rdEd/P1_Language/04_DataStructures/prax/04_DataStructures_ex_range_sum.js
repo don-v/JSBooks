b84143ad6ae46e6da63961cec3114887f9947ab2
don-v <54256066+don-v@users.noreply.github.com>
2023-12-26 12:25:32 -0500

    EloquentJS: C4 -- ex sum or range, contd!

--- EloquentJS_3rdEd/P1_Language/04_DataStructures/prax/04_DataStructures_ex_range_sum.js
+++ EloquentJS_3rdEd/P1_Language/04_DataStructures/prax/04_DataStructures_ex_range_sum.js
@@ -30,19 +30,37 @@ function range1(start, end) {
     return x;
 }
 
-function range2(start, end, step) {
+
+function sum1(arr) {
+    total = 0;
+    for (let x of arr) {
+        total += x;
+    }
+    return total
+}
+
+
+function range2(start, end, step=1) {
     let x = [start]
-    for (let index = start + 1; index <= end; index++) {
-        x.push(index);
+    if (step > 0) {
+        for (let index = start + step; index <= end; index += step) {
+            x.push(index);
+        }    
+    } else {
+        for (let index = start + step; index >= end; index += step) {
+            x.push(index);
+        }
     }
     return x;
 }
 
 
-console.log(range1(1, 10));
+console.log('console.log(range1(1, 10));:',range1(1, 10));
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-// console.log(range(5, 2, -1));
+console.log('console.log(range2(5, 2, -1));:',range2(5, 2, -1));
 // → [5, 4, 3, 2]
-//console.log(sum(range(1, 10)));
+console.log('console.log(range2(1, 10, 2));:',range2(1, 10, 2));
+// → [ 1, 3, 5, 7, 9 ]
+console.log('console.log(sum1(range1(1, 10)));:',sum1(range1(1, 10)));
 // → 55
 
