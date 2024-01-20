99ecc4261e802fa222375d58feddfd946c9d29ca
don-v <54256066+don-v@users.noreply.github.com>
2024-01-20 13:49:26 -0500

    EloquentJS: C4 -- deep comparison ex!

--- EloquentJS_3rdEd/P1_Language/04_DataStructures/notes/04_DataStructures.md
+++ EloquentJS_3rdEd/P1_Language/04_DataStructures/notes/04_DataStructures.md
@@ -1384,4 +1384,39 @@ console.log(nth(arrayToList([10, 20, 30]), 1));
 
 ## DEEP COMPARISON
 
+The `==` operator compares objects by identity. But sometimes, one'd prefer to
+compare the values of their actual properties.
+
+Write a function `deepEqual` that takes two values and returns true only if they
+are the same value or are object with the same properties, where the values of 
+the properties are equal when compared with a recursive call to `deepEqual`
+
+To find out whether values should be compared direclty (use the `===` operator
+for that) or have thier properties compared, one can use the `typeof` operator.
+
+If it produces `"object"`, for both values, one should do a deep comparison. But
+one has to take one silly exception into account: because of a historical accident, 
+`typeof null` also produces `"object"`.
+
+The `Object.keys` function will be useful when one needs to go over the properties
+of object to compare them.
+
+```js
+// Your code here.
+
+let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
+```
+
+### MY SOLUTION
+
+### DISPLAY HINTS
+
+### TEACH'S SOLUTION
+
 <!-- 'DEEP COMPARISON EX! -->
---
+++ EloquentJS_3rdEd/P1_Language/04_DataStructures/prax/04_DataStructures_ex_deep_comparison.js
@@ -0,0 +1,39 @@
+/* 
+The `==` operator compares objects by identity. But sometimes, one'd prefer to
+compare the values of their actual properties.
+
+Write a function `deepEqual` that takes two values and returns true only if they
+are the same value or are object with the same properties, where the values of 
+the properties are equal when compared with a recursive call to `deepEqual`
+
+To find out whether values should be compared direclty (use the `===` operator
+for that) or have thier properties compared, one can use the `typeof` operator.
+
+If it produces `"object"`, for both values, one should do a deep comparison. But
+one has to take one silly exception into account: because of a historical accident, 
+`typeof null` also produces `"object"`.
+
+The `Object.keys` function will be useful when one needs to go over the properties
+of object to compare them.
+*/
+
+
+// Your code here.
+
+function deepEqual(o1, o2) {
+    let x = typeof o1;
+    console.log('typeof o1: ',x);
+    console.log('typeof x', typeof x);
+    console.log('typeof o2: ',typeof o2);
+}
+
+
+
+let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
+
