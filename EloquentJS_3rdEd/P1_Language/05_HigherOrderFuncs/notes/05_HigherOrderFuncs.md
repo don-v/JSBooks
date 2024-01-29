1e93a86a12517c9a1832a9e499928e4aefd54ccf
don-v <54256066+don-v@users.noreply.github.com>
2024-01-29 17:57:07 -0500

    EloquentJS: C5 -- higher order funcs!

--- EloquentJS_3rdEd/P1_Language/05_HigherOrderFuncs/notes/05_HigherOrderFuncs.md
+++ EloquentJS_3rdEd/P1_Language/05_HigherOrderFuncs/notes/05_HigherOrderFuncs.md
@@ -172,4 +172,45 @@ console.log(greaterThan10(11));
 // → true
 ```
 
+And, we cna have functions that change other functions:
+
+```js
+function noisy(f) {
+  return (...args) => {
+    console.log("calling with", args);
+    let result = f(...args);
+    console.log("called with", args, ", returned", result);
+    return result;
+  };
+}
+noisy(Math.min)(3, 2, 1);
+// → calling with [3, 2, 1]
+// → called with [3, 2, 1] , returned 1
+```
+
+We can even write functions that provide new types of control flow.
+
+```js
+function unless(test, then) {
+  if (!test) then();
+}
+
+repeat(3, n => {
+  unless(n % 2 == 1, () => {
+    console.log(n, "is even");
+  });
+});
+// → 0 is even
+// → 2 is even
+```
+
+There is a built-in array method, `forEach`, that provides something like a 
+`for of` loop as a higher-order function.
+
+```js
+["A", "B"].forEach(l => console.log(l));
+// → A
+// → B
+```
+
 <!-- HERE -- HIGHER-ORDER FUNCTIONS! -->
--- EloquentJS_3rdEd/P1_Language/05_HigherOrderFuncs/prax/05_HigherOrderFuncs_HigherOrder_Funcs.js
+++ EloquentJS_3rdEd/P1_Language/05_HigherOrderFuncs/prax/05_HigherOrderFuncs_HigherOrder_Funcs.js
@@ -13,3 +13,65 @@ let x = `function greaterThan(n) {
   let greaterThan10 = greaterThan(10);
   console.log(greaterThan10(11));
   // → true
+
+x = `function noisy(f) {
+  return (...args) => {
+    console.log("calling with", args);
+    let result = f(...args);
+    console.log("called with", args, ", returned", result);
+    return result;
+  };
+}
+noisy(Math.min)(3, 2, 1);
+// → calling with [3, 2, 1]
+// → called with [3, 2, 1] , returned 1`;
+
+console.log(x);
+
+function noisy(f) {
+  return (...args) => {
+    console.log("calling with", args);
+    let result = f(...args);
+    console.log("called with", args, ", returned", result);
+    return result;
+  };
+}
+noisy(Math.min)(3, 2, 1);
+// → calling with [3, 2, 1]
+// → called with [3, 2, 1] , returned 1
+
+x = `function unless(test, then) {
+  if (!test) then();
+}
+
+repeat(3, n => {
+  unless(n % 2 == 1, () => {
+    console.log(n, "is even");
+  });
+});
+// → 0 is even
+// → 2 is even`;
+
+console.log(x);
+
+function unless(test, then) {
+  if (!test) then();
+}
+
+repeat(3, n => {
+  unless(n % 2 == 1, () => {
+    console.log(n, "is even");
+  });
+});
+// → 0 is even
+// → 2 is even
+
+x = `["A", "B"].forEach(l => console.log(l));
+// → A
+// → B`;
+
+console.log(x);
+
+["A", "B"].forEach(l => console.log(l));
+// → A
+// → B
