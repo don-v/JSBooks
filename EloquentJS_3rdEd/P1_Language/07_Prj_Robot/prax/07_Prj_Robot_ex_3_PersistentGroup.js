c5fd754ea79fb96609a35119dfa3cd4d39deddf5
don-v <54256066+don-v@users.noreply.github.com>
2025-01-02 10:26:27 -0500

    EloquentJS: C7 -- ex3 persistent group contd!

--- EloquentJS_3rdEd/P1_Language/07_Prj_Robot/prax/07_Prj_Robot_ex_3_PersistentGroup.js
+++ EloquentJS_3rdEd/P1_Language/07_Prj_Robot/prax/07_Prj_Robot_ex_3_PersistentGroup.js
@@ -1,3 +1,42 @@
+// checking teach ka `Group` class C6:
+
+class Group1 {
+  #members = [];
+
+  add(value) {
+    if (!this.has(value)) {
+      this.#members.push(value);
+    }
+  }
+
+  delete(value) {
+    this.#members = this.#members.filter(v => v !== value);
+  }
+
+  has(value) {
+    return this.#members.includes(value);
+  }
+
+  static from(collection) {
+    let group = new Group1;
+    for (let value of collection) {
+      group.add(value);
+    }
+    return group;
+  }
+}
+
+let group = Group1.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// → false
+
+
 
 // TEACH KA `Group` class solution from C6:
 
@@ -105,13 +144,13 @@ class PGroup {
 
 }
 
-let a = PGroup.empty.add("a");
-let ab = a.add("b");
-let b = ab.delete("a");
+// let a = PGroup.empty.add("a");
+// let ab = a.add("b");
+// let b = ab.delete("a");
 
-console.log(b.has("b"));
-// → true
-console.log(a.has("b"));
-// → false
-console.log(b.has("a"));
-// → false
+// console.log(b.has("b"));
+// // → true
+// console.log(a.has("b"));
+// // → false
+// console.log(b.has("a"));
+// // → false
