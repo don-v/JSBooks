e4e9aae9d895f28f90e11390f23c57d37d1cc3b7
don-v <54256066+don-v@users.noreply.github.com>
2025-01-08 08:57:30 -0500

    EloquentJS: C7 -- ex3 persistent group contd!

--- EloquentJS_3rdEd/P1_Language/07_Prj_Robot/prax/07_Prj_Robot_ex_3_PersistentGroup.js
+++ EloquentJS_3rdEd/P1_Language/07_Prj_Robot/prax/07_Prj_Robot_ex_3_PersistentGroup.js
@@ -146,11 +146,11 @@ class PGroup {
 }
 
 let a = PGroup.empty.add("a");
-// let ab = a.add("b");
-// let b = ab.delete("a");
+let ab = a.add("b");
+let b = ab.delete("a");
 
-// console.log(b.has("b"));
-// // → true
+console.log(b.has("b"));
+// → true
 // console.log(a.has("b"));
 // // → false
 // console.log(b.has("a"));
