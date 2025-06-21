dead01a451c21d049847095e8459194807805111
don-v <54256066+don-v@users.noreply.github.com>
2025-06-21 15:39:42 -0400

    EloquentJS: C9 -- ex3: js numbers!

--- EloquentJS_3rdEd/P1_Language/09_RegularExpressions/prax/09_RegularExpressions_ex3_numbes_again.js
+++ EloquentJS_3rdEd/P1_Language/09_RegularExpressions/prax/09_RegularExpressions_ex3_numbes_again.js
@@ -9,18 +9,37 @@ but the number cannot be a dot alone. That is, .5 and 5. are valid JS numbers, b
 // let number = /^...$/;
 
 
-let number = /^\+?|^-?\d*\.?e?\+|-?\d*(?:\.\d)?/gi;
+// let number = /^\+?|^-?\d*\.?e?\+|-?\d*(?:\.\d)?/gi;
+let number = /\+?|-?\d*?\.?\d*?/gi;
+
 
 // Tests:
-for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
-                 "1.3e2", "1E-4", "1e+12"]) {
+for (let str of [
+  "1"
+  ,
+  "-1"
+  ,
+  "+15"
+  ,
+  "1.55"
+  // ,
+  // ".5"
+  // ,
+  // "5."
+  // ,
+  // "1.3e2"
+  // ,
+  // "1E-4"
+  // ,
+  // "1e+12"
+]) {
   if (!number.test(str)) {
     console.log(`Failed to match '${str}'`);
   }
 }
-for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
-                 ".5.", "1f5", "."]) {
-  if (number.test(str)) {
-    console.log(`Incorrectly accepted '${str}'`);
-  }
-}
+// for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+//                  ".5.", "1f5", "."]) {
+//   if (number.test(str)) {
+//     console.log(`Incorrectly accepted '${str}'`);
+//   }
+// }
