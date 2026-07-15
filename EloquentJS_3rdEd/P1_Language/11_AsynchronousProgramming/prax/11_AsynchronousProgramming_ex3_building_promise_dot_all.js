8a74dcb12d2517ec7d74cf4eb2572fc8383c3b68
don-v <54256066+don-v@users.noreply.github.com>
2026-07-15 12:04:08 -0400

    EloquentJS: C11 -- ex3!

--- EloquentJS_3rdEd/P1_Language/11_AsynchronousProgramming/prax/11_AsynchronousProgramming_ex3_building_promise_dot_all.js
+++ EloquentJS_3rdEd/P1_Language/11_AsynchronousProgramming/prax/11_AsynchronousProgramming_ex3_building_promise_dot_all.js
@@ -12,11 +12,8 @@ handles a failure of one's promise.
 
 function Promise_all(promises) {
   return new Promise((resolve, reject) => {
-    results = []
-    if (promises) {
-      
-    }
-    
+    results = [];
+    resolve(results);
   });
 }
 
@@ -24,14 +21,22 @@ function Promise_all(promises) {
 Promise_all([]).then(array => {
   console.log("This should be []:", array);
 });
-// function soon(val) {
-//   return new Promise(resolve => {
-//     setTimeout(() => resolve(val), Math.random() * 500);
-//   });
-// }
+function soon(val) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(val), Math.random() * 500);
+  });
+}
 // Promise_all([soon(1), soon(2), soon(3)]).then(array => {
 //   console.log("This should be [1, 2, 3]:", array);
 // });
+
+Promise.all([soon(1), soon(2), soon(3)]).then(array => {
+  console.log("This should be [1, 2, 3]:", array);
+});
+
+
+
+
 // Promise_all([soon(1), Promise.reject("X"), soon(3)])
 //   .then(array => {
 //     console.log("We should not get here");
